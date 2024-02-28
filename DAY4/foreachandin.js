111f94b747a959aa14b53cb3f8aebadfd0585bd6
uwinezaflorence20 <uwinezaflorence20@gmail.com>
2024-02-28 11:45:12 +0200

    well

---
+++ DAY4/foreachandin.js
@@ -0,0 +1,25 @@
+let Obj = {
+    citizens: {
+        men: 10,
+        women:3
+    },
+    house: {
+        bed: "made in rwanda",
+        desk: "made in usa"
+    }
+};
+console.log(Obj.citizens.men);
+console.log(Obj.citizens.women);
+console.log(Obj.house.bed);
+console.log(Obj.house.desk);
+
+
+
+
+let arr=[
+    [1,2,3,4,5],
+    ["a","b","c","d","e"],
+    [6,7,8,9,10],
+];
+console.log(arr[2][4]);
+console.log(arr[1][4]);
