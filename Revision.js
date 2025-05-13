107670707e57a34c81a48f55a2ecb589425e4190
celine <celine.himbaza@gmail.com>
2025-05-13 16:18:35 +0200

    done

---
+++ Revision.js
@@ -0,0 +1,32 @@
+let myName = "uwineza";
+let myAge = 20;
+let myBod = "20/05/200";
+console.log("Hello Everybody my name is "+myName+ " and my age is " +myAge+ "and my birthday is " +myBod);
+function testfunction(){
+    console.log("Hello people");
+}
+testfunction();
+
+//six variable example
+
+let rosePrice =8;
+let lilyPrice = 10;
+let tuliPrice =2;
+
+let roseQuantity=10;
+let lilyQuantity=70;
+let tuliQuantity = 50;
+
+let roseValue = rosePrice*roseQuantity;
+let lilyValue = lilyPrice*lilyQuantity;
+let tuliValue = tuliQuantity*tuliPrice;
+
+
+let totalValue = roseValue+lilyValue+tuliValue;
+
+console.log("Rose – unit price:", rosePrice, ", quantity:", roseQuantity, ", value:", roseValue);
+console.log("Lily – unit price:", lilyPrice, ", quantity:", lilyQuantity, ", value:", lilyValue);
+console.log("Tulip – unit price:", tuliPrice, ", quantity:", tuliQuantity, ", value:", tuliValue);
+console.log("Total: ", totalValue);
+
+//
